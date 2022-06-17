fecf91f062b6ab4fe63084c27f8b87105822c964
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-18 00:18:19 +0300

    Added our custom user model to the admin with our custom configuration.

--- ryhom/accounts/admin.py
+++ ryhom/accounts/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import UserProfile
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        (_('Personal Info'), {'fields': ('name', 'gender', 'avatar', 'bio',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined',)}),
+    )
+    readonly_fields = ['last_login', 'date_joined']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+admin.site.register(UserProfile, UserAdmin)
