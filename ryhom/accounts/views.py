609d3208a6a3dc6cd0ca13d95aac7efb595f7eb6
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-19 00:36:18 +0300

    Added view for user registration.

--- ryhom/accounts/views.py
+++ ryhom/accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+#from django.conf import settings
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import RegisterUserForm
+
+
+class RegisterView(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = RegisterUserForm
+    #success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
+    success_url = 'index.html'
