081ceb2b5a4508813705beee013a7ac49129452d
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-20 23:50:45 +0300

    Removed username from the BaseUserManager.

--- ryhom/accounts/managers.py
+++ ryhom/accounts/managers.py
@@ -3,14 +3,11 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, name, username, email,
-                    password=None, **extra_fields):
+    def create_user(self, name, email, password=None, **extra_fields):
         """Create, save and return a new user."""
 
         if not name:
             raise ValueError('All users must have a name!')
-        if not username:
-            raise ValueError('All users must have a username!')
         if not email:
             raise ValueError('All users must have an email address!')
 
@@ -18,7 +15,6 @@ class UserManager(BaseUserManager):
 
         user = self.model(
             name=name,
-            username=username,
             email=email,
             **extra_fields
         )
@@ -29,11 +25,10 @@ class UserManager(BaseUserManager):
         return user
 
 
-    def create_superuser(self, email, password, name, username):
+    def create_superuser(self, email, password, name):
         """Create and return a new superuser."""
         user = self.create_user(
             name=name,
-            username=username,
             email=email,
             password=password,
         )
