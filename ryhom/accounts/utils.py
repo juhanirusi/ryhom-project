06fb7c1159824830b2e601669214ab02ea35dbbf
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-20 23:52:12 +0300

    Created the token generator for the email activation link.

---
+++ ryhom/accounts/utils.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from six import text_type
+
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    """
+    Generate a token that will be added to the confirmation email
+    that's sended to the user after successful registration.
+    """
+    def _make_hash_value(self, user, timestamp):
+        return (
+            text_type(user.pk) + text_type(timestamp) +
+            text_type(user.is_active) # Activating account after URL click
+        )
+
+account_activation_token = AccountActivationTokenGenerator()
