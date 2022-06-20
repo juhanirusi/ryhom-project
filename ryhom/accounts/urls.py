5b3acd85edc75177a98d0140ef9d4ce98b376788
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-20 23:51:15 +0300

    Added URLs for activating the account and for logging in.

--- ryhom/accounts/urls.py
+++ ryhom/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import RegisterView
+from .views import ActivateAccountView, RegisterView, login_user
 
 urlpatterns = [
-    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/create-account/', RegisterView.as_view(), name='create-account'),
+    path('activate/<uidb64>/<token>/', ActivateAccountView.as_view(), name='activate'),
+
+    path('auth/login/', login_user, name='login'),
 ]
