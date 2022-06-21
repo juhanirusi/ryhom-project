84fd36f1e0763e63e444ab0f51579dadc260f011
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-22 01:31:14 +0300

    Edited account activation view & added login view.

--- ryhom/accounts/views.py
+++ ryhom/accounts/views.py
@@ -1,6 +1,7 @@
-#from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
+#from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
 from django.shortcuts import redirect, render
@@ -11,16 +12,15 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.views.generic import View
 from django.views.generic.edit import CreateView
 
-from .forms import RegisterUserForm
-from .models import UserProfile
-from .utils import account_activation_token
+from .forms import LoginForm, RegisterForm
+from .models import Account
+from .utils import account_token_generator
 
 
 class RegisterView(CreateView):
 
     template_name = 'accounts/create-account.html'
-    form_class = RegisterUserForm
-    #success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
+    form_class = RegisterForm
     success_url =  reverse_lazy('create-account')
 
 
@@ -37,7 +37,7 @@ class RegisterView(CreateView):
             'user': user,
             'domain': current_site.domain,
             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            'token': account_activation_token.make_token(user),
+            'token': account_token_generator.make_token(user),
         })
 
         to_email = form.cleaned_data['email']
@@ -61,37 +61,87 @@ class RegisterView(CreateView):
 
 
 class ActivateAccountView(View):
-    def get(self, request, uidb64, token):
+    def get(self, request, uidb64, token,
+            backend='django.contrib.auth.backends.ModelBackend'):
         try:
             uid = force_str(urlsafe_base64_decode(uidb64))
-            user = UserProfile.objects.get(pk=uid)
-        except (TypeError, ValueError, OverflowError, UserProfile.DoesNotExist):
+            user = Account.objects.get(pk=uid)
+        except (TypeError,
+                ValueError,
+                OverflowError,
+                Account.DoesNotExist):
+
             user = None
 
-        if user is not None and account_activation_token.check_token(user, token):
+        if user is not None and account_token_generator.check_token(
+            user, token):
+
             user.is_active = True
             user.save()
-            login(request, user)
-            messages.success(request, 'GREAT! NOW LOGIN!')
-            return redirect('accounts/create-account.html')
+
+            if request.user.is_authenticated:
+                logout(request)
+
+            login(
+                request,
+                user,
+                backend
+            )
+            messages.success(request, 'Email verfied!')
+            # CHANGE TO PROFILE PAGE IN THE FUTURE!
+            return redirect('index')
         else:
             messages.warning(request, ('The confirmation link was invalid, \
                                 possibly because it has already been used.'))
-            return redirect('accounts/create-account.html')
+            return redirect('index')
 
 
-def login_user(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            messages.success(request, ('Antamasi salasana tai käyttäjätunnus on väärä!'))
-            return redirect('accounts/login.html')
-    else:
-        if request.user.is_authenticated:
-            return redirect('/')
-        return render(request, 'accounts/login.html')
+# def login_user(request):
+#     if request.method == 'POST':
+#         email = request.POST['email']
+#         password = request.POST['password']
+#         user = authenticate(request, username=email, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('index')
+#         else:
+#             messages.success(request, ('Antamasi salasana tai käyttäjätunnus on väärä!'))
+#             return redirect('login')
+#     else:
+#         if request.user.is_authenticated:
+#             return redirect('index')
+#         return render(request, 'accounts/login.html')
+
+
+
+
+
+
+
+class LoginView(LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = LoginForm
+    next_page = 'index' # CHANGE IN THE FUTURE!
+    redirect_authenticated_user = 'index' # CHANGE IN THE FUTURE!
+
+    # def form_invalid(self, form):
+    #     """If the form is invalid, render the invalid form."""
+    #     return self.render_to_response(self.get_context_data(form=form))
+
+    # def get(self, request):
+    #     form = self.form_class
+    #     messages.success(request, 'JEEEEEE!')
+    #     return render(request, self.template_name, context={'form': form})
+
+    # def post(self, request):
+    #     form = self.form_class(request.POST)
+    #     if form.is_valid():
+    #         user = authenticate(
+    #             email=form.cleaned_data['email'],
+    #             password=form.cleaned_data['password'],
+    #         )
+    #         if user is not None:
+    #             login(request, user)
+    #             return redirect('index')
+    #     message = 'Login failed!'
+    #     return render(request, self.template_name, context={'form': form, 'message': message})
