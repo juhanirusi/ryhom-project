867243b1ef9534208605244325957d3ed40b61ff
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-22 01:29:24 +0300

    Added custom backend code to let users log in with both email & username.

---
+++ ryhom/accounts/backends.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
+from django.db.models import Q
+
+User = get_user_model()
+
+
+class EmailUsernameAuthBackend(object):
+    def authenticate(self, request, username=None, email=None, password=None):
+        try:
+            user = User.objects.get(
+                Q(email=email) | Q(username=username)
+            )
+
+        except User.DoesNotExist:
+            return None
+
+        if user and check_password(password, user.password):
+            return user
+
+        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
