c66d53866f79aa0bdfa68f4fecbc0bb3600111b8
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-22 01:29:44 +0300

    Added login form.

--- ryhom/accounts/forms.py
+++ ryhom/accounts/forms.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.exceptions import ValidationError
 
 from .models import Account
 
 
-class RegisterUserForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -23,3 +24,37 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = Account
         fields = ('email', 'name', 'password1', 'password2')
+
+
+class LoginForm(AuthenticationForm):
+    """
+    Our custom login form that subclasses Django's AuthenticationForm,
+    but also includes our custom placeholders, labels and an error
+    message for users who haven't verified their email address.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter email or username'
+        self.fields['password'].widget.attrs['placeholder'] = 'Enter password'
+
+        self.fields['username'].label = 'Email or username'
+
+    class Meta:
+        model = Account
+        fields = ('email', 'password')
+
+
+    def confirm_login_allowed(self, user):
+        """
+        Let users who haven't clicked their email verification
+        link know that they can't log into their account until
+        verifying the account.
+        """
+        if not user.is_active:
+            raise ValidationError('Your account is still inactive! \
+                Click the confirmation link sent to your email to \
+                activate it.',
+                code='mycode'
+            )
