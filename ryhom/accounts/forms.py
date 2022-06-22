aae5c65b258684e4e71377fe1d276a6e67374958
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-23 00:59:38 +0300

    Created account settings form.

--- ryhom/accounts/forms.py
+++ ryhom/accounts/forms.py
@@ -1,15 +1,28 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from datetime import date
+
+from django.contrib.auth.forms import (AuthenticationForm, UserChangeForm,
+                                       UserCreationForm)
 from django.core.exceptions import ValidationError
+from django.forms import SelectDateWidget, Textarea
+from django.utils.translation import gettext_lazy as _
 
 from .models import Account
 
 
 class RegisterForm(UserCreationForm):
+    """
+    Our custom registration form that subclasses Django's
+    UserCreationForm, but also includes our custom
+    placeholders, labels and help texts.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields['email'].widget.attrs['placeholder'] = 'Enter email'
-        self.fields['name'].widget.attrs['placeholder'] = 'First and last name'
+        self.fields['name'].widget.attrs[
+            'placeholder'
+        ] = 'First and last name'
         self.fields['password1'].widget.attrs['placeholder'] = '********'
         self.fields['password2'].widget.attrs['placeholder'] = '********'
 
@@ -19,7 +32,7 @@ class RegisterForm(UserCreationForm):
         for fieldname in ['email', 'name', 'password1', 'password2']:
             self.fields[fieldname].help_text = None
 
-        self.fields['password1'].help_text = 'Your password must contain at least 8 characters.'
+        self.fields['password1'].help_text = 'Password must contain at least 8 characters.'
 
     class Meta:
         model = Account
@@ -56,5 +69,70 @@ class LoginForm(AuthenticationForm):
             raise ValidationError('Your account is still inactive! \
                 Click the confirmation link sent to your email to \
                 activate it.',
-                code='mycode'
+                code='inactive'
             )
+
+
+class AccountSettingsForm(UserChangeForm):
+    """
+    Our custom account settings form that subclasses Django's UserChangeForm,
+    but also includes our custom placeholders, labels and a clean method to
+    make sure that user's can't submit username's shorter than 3 characters.
+
+    The birthday field has also been edited, so that user's can only choose
+    reasonable years, for example user's can't be 200 or 1 year olds.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs[
+            'placeholder'
+        ] = 'First and last name'
+        self.fields['bio'].widget.attrs['placeholder'] = 'Eat, sleep, garden.'
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter username'
+
+    password = None
+
+    class Meta:
+        model = Account
+        fields = ('name', 'username', 'email', 'gender',
+                'birthday', 'profile_image', 'bio',
+        )
+
+        labels = {
+            'name'              : 'Full Name',
+            'bio'               : 'Short Bio',
+            'profile_image'     : 'Profile Image',
+        }
+
+        date_labels = ('Year', 'Month', 'Day')
+        MONTHS = {
+            1:_('Jan'), 2:_('Feb'), 3:_('Mar'), 4:_('Apr'),
+            5:_('May'), 6:_('Jun'), 7:_('Jul'), 8:_('Aug'),
+            9:_('Sep'), 10:_('Oct'), 11:_('Nov'), 12:_('Dec')
+        }
+        this_year = date.today().year
+        year_range = [_ for _ in range(this_year - 100, this_year - 15)]
+
+        widgets = {
+            'birthday': SelectDateWidget(
+                empty_label=date_labels,
+                months=MONTHS,
+                years=year_range
+            ),
+            'bio': Textarea(attrs={"rows":5, "cols":35})
+        }
+
+    def clean_username(self):
+        """
+        Validate the username field to ensure that the field
+        can't be empty & has to be longer than 3 characters.
+        """
+        username = self.cleaned_data['username']
+
+        if len(username) < 3:
+            raise ValidationError(
+                'Give yourself a username that is at least 3 characters.'
+            )
+        return username
