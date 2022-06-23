6c644e83d8ae9ed88190ad6466a9e076f71b5106
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-24 00:26:46 +0300

    Added user profile URL and written rest of the URLs more readable.

--- ryhom/accounts/urls.py
+++ ryhom/accounts/urls.py
@@ -1,16 +1,40 @@
 from django.urls import path
 
 from .views import (AccountSettingsView, ActivateAccountView, LoginUserView,
-                    LogoutUserView, RegisterView)
+                    LogoutUserView, RegisterView, UserProfileView)
+
+app_name = 'accounts'
 
 urlpatterns = [
-    path('auth/create-account/', RegisterView.as_view(),
-        name='create_account'),
-    path('auth/activate/<uidb64>/<token>/', ActivateAccountView.as_view(),
-        name='activate'),
-    path('auth/login/', LoginUserView.as_view(), name='login'),
-    path('auth/logout/', LogoutUserView.as_view(), name='logout'),
+    path(
+        'auth/create-account/',
+        RegisterView.as_view(),
+        name='create_account'
+    ),
+    path(
+        'auth/activate/<uidb64>/<token>/',
+        ActivateAccountView.as_view(),
+        name='activate'
+    ),
+    path(
+        'auth/login/',
+        LoginUserView.as_view(),
+        name='login'
+    ),
+    path(
+        'auth/logout/',
+        LogoutUserView.as_view(),
+        name='logout'
+    ),
 
-    path('account/settings/', AccountSettingsView.as_view(),
-        name='account_settings'),
+    path(
+        'account/settings/',
+        AccountSettingsView.as_view(),
+        name='account_settings'
+    ),
+    path(
+        '<slug:slug>',
+        UserProfileView.as_view(),
+        name='user_profile'
+    ),
 ]
