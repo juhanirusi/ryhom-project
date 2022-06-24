86eb30bebc55b8f72ecab684580a444f55b87701
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-25 00:19:59 +0300

    Added condition for showing admin URLs with environment variable.

--- config/urls.py
+++ config/urls.py
@@ -8,11 +8,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
     path('', include('ryhom.layout.urls', namespace='layout')),
     path('', include('ryhom.accounts.urls', namespace='accounts')),
 ]
+
+if settings.IS_ADMIN_ENABLED:
+    urlpatterns.append(path('admin/', admin.site.urls))
 
 if settings.DEBUG:
     urlpatterns += [
