49af2c0b9fb5df5bb56b4e0c37a21bfb48673179
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-27 00:08:22 +0300

    Added 404 handler + removed layout app and included core app urls.

--- config/urls.py
+++ config/urls.py
@@ -6,11 +6,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from ryhom.core.views import Error404View
 
 urlpatterns = [
-    path('', include('ryhom.layout.urls', namespace='layout')),
+    path('', include('ryhom.core.urls', namespace='core')),
     path('', include('ryhom.accounts.urls', namespace='accounts')),
 ]
+
 
 if settings.IS_ADMIN_ENABLED:
     urlpatterns.append(path('admin/', admin.site.urls))
@@ -19,3 +21,6 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = Error404View.as_view()
