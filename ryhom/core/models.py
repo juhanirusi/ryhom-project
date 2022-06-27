f1766c03489a0a60dd39d63d6ef15e0139151239
JuhaniRusi <juhanirusi@yahoo.com>
2022-06-28 02:09:34 +0300

    Added a abstract model to core app.

--- ryhom/core/models.py
+++ ryhom/core/models.py
@@ -1,27 +1,28 @@
-# from django.db import models
-# import uuid
+import uuid
 
-# LET'S ADD A BASE MODEL HERE THAT WE'RE GOING TO USE ACROSSS
-# OUR OTHER MODELS. FOR EXAMPLE, CODE LIKE THIS...
+from django.db import models
 
-# class BaseAbstractModel(models.Model):
 
-#     """
-#      This model defines base models that implements common fields like:
-#      created_at
-#      updated_at
-#      is_deleted
-#     """
-#     uuid = models.UUIDField(default=uuid.uuid4, editable=False, null=False, unique=True)
-#     created_at = models.DateTimeField(auto_now_add=True, editable=False)
-#     updated_at = models.DateTimeField(auto_now=True, editable=False)
-#     is_deleted = models.BooleanField(default=False)
+class BaseAbstractModel(models.Model):
+    """
+    Our base model that we can subclass from when creating models.
+    This model contains common fields that
+    we tend to use on most of our models.
 
-#     def soft_delete(self):
-#         """soft  delete a model instance"""
-#         self.is_deleted=True
-#         self.save()
+    Our model also contains an UUID field. We'll keep Django's own
+    sequential id as primary key, but add an additional UUID field
+    to the model because it's going to be a safer method for public
+    model lookups like APIs.
+    """
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        null=False,
+        unique=True,
+        db_index=True
+    )
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
 
-#     class Meta:
-#         abstract = True
-#         ordering = ['-created_at']
+    class Meta:
+        abstract = True
