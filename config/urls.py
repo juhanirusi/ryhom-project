7737a3b98fa3a3df8a9b1a74950f69870b1b71c9
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-01 01:23:22 +0300

    Added app URLs to main urls.py

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,12 @@ from ryhom.core.views import Error404View
 urlpatterns = [
     path('', include('ryhom.core.urls', namespace='core')),
     path('', include('ryhom.accounts.urls', namespace='accounts')),
+    path('', include('ryhom.categories.urls', namespace='categories')),
+    path('', include('ryhom.tags.urls', namespace='tags')),
+    path('', include('ryhom.articles.urls', namespace='articles')),
+    path('', include('ryhom.microposts.urls', namespace='microposts')),
+
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 
