2235919e0e41bf1a42ecfa23067747b27fcd6e36
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-04 01:16:40 +0300

    URL paths for article detail view & a view where users can see their own posts.

--- ryhom/articles/urls.py
+++ ryhom/articles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import AddArticleView
+from .views import AddArticleView, ArticleDetailView, UserPostsView
 
 app_name = 'articles'
 
 urlpatterns = [
     path('add-article/', AddArticleView.as_view(), name='add_article'),
+    path('my-posts/', UserPostsView.as_view(), name='my_posts'),
+    path('<slug:author>/<slug:slug>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
