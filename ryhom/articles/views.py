942061dc67b1f051046ed1a04f2dcd2a5fcd6753
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-05 00:15:50 +0300

    Added commenting functionality to article detail view.

--- ryhom/articles/views.py
+++ ryhom/articles/views.py
@@ -1,12 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView
 
-from .forms import AddArticleForm
-from .models import Article
+from .forms import AddArticleForm, AddCommentForm
+from .models import Article, Comment
 
 
 class AddArticleView(LoginRequiredMixin, CreateView):
@@ -63,9 +63,6 @@ class UserPostsView(LoginRequiredMixin, ListView):
 class ArticleDetailView(DetailView):
     template_name = 'articles/article-detail.html'
     model = Article
-    # slug_field = 'slug'
-    # slug_url_kwarg = 'slug'
-    # query_pk_and_slug = True
     queryset = Article.objects.filter(status='Published')
 
     # def get_context_data(self, **kwargs):
@@ -74,8 +71,94 @@ class ArticleDetailView(DetailView):
     #     context['slug'] = Article.objects.filter(slug=self.object.slug).first()
     #     return context
 
+    def get_context_data(self , **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        connected_comments = Comment.objects.filter(article=self.get_object())
+        number_of_comments = connected_comments.count()
+
+        context['comments'] = connected_comments
+        context['nro_of_comments'] = number_of_comments
+        context['comment_form'] = AddCommentForm()
+        return context
+
     def get_object(self):
         return get_object_or_404(self.queryset,
             author__slug=self.kwargs['author'],
             slug=self.kwargs['slug']
         )
+
+    def post(self , request , *args , **kwargs):
+        if self.request.method == 'POST':
+            form = AddCommentForm(self.request.POST)
+            if form.is_valid():
+                comment = form.cleaned_data['comment']
+                try:
+                    parent = form.cleaned_data['parent']
+                except:
+                    parent=None
+
+            new_comment = Comment(comment=comment , author=self.request.user , article=self.get_object() , parent=parent)
+            new_comment.save()
+            return redirect(self.request.path_info)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class AddCommentLike(LoginRequiredMixin, View):
+#     def post(self, request, post_pk, pk, *args, **kwargs):
+#         comment = Comment.objects.get(pk=pk)
+#         is_dislike = False
+#         for dislike in comment.dislikes.all():
+#             if dislike == request.user:
+#                 is_dislike = True
+#                 break
+#         if is_dislike:
+#             comment.dislikes.remove(request.user)
+#         is_like = False
+#         for like in comment.likes.all():
+#             if like == request.user:
+#                 is_like = True
+#                 break
+#         if not is_like:
+#             comment.likes.add(request.user)
+
+#         if is_like:
+#             comment.likes.remove(request.user)
+#         next = request.POST.get('next', '/')
+#         return HttpResponseRedirect(next)
+# class AddCommentDislike(LoginRequiredMixin, View):
+#     def post(self, request, post_pk, pk, *args, **kwargs):
+#         comment = Comment.objects.get(pk=pk)
+#         is_like = False
+#         for like in comment.likes.all():
+#             if like == request.user:
+#                 is_like = True
+#                 break
+#         if is_like:
+#             comment.likes.remove(request.user)
+#         is_dislike = False
+#         for dislike in comment.dislikes.all():
+#             if dislike == request.user:
+#                 is_dislike = True
+#                 break
+#         if not is_dislike:
+#             comment.dislikes.add(request.user)
+
+#         if is_dislike:
+#             comment.dislikes.remove(request.user)
+#         next = request.POST.get('next', '/')
+#         return HttpResponseRedirect(next)
