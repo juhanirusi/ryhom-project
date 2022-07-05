a9c9159128180ad3dc59433cdf960cec0012a39f
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-06 00:32:23 +0300

    Added a view for specific categories and listing posts that belong to that specific category.

--- ryhom/categories/views.py
+++ ryhom/categories/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from ryhom.articles.models import Article
 
-# Create your views here.
+from .models import Category
+
+
+class CategoryDetailView(ListView):
+    model = Category
+    context_object_name = 'category'
+    template_name = 'categories/category-page.html'
+
+    def get_queryset(self):
+        category = Category.objects.get(slug=self.kwargs['category_slug'])
+        self.queryset = Article.objects.filter(categories=category)
+        return category
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryDetailView, self).get_context_data(**kwargs)
+        context['posts_in_category'] = self.queryset
+        return context
