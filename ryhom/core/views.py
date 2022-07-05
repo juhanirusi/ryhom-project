74e5a429eeb32c28d03261abcc16f1666e23115c
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-06 00:32:47 +0300

    Moved user_profile_redirect view into core app.

--- ryhom/core/views.py
+++ ryhom/core/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from django.shortcuts import redirect
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import UpdateView
@@ -24,3 +26,15 @@ class ConfirmPasswordView(UpdateView):
 class Error404View(View):
     def index(self):
         raise Http404
+
+
+@login_required
+def user_profile_redirect(request):
+    """
+    Used as a dynamic redirect to user's personal profile, because the
+    user profile URL requires a slug kwarg, hence, a custom redirect
+
+    settings.py --> LOGIN_REDIRECT_URL
+    """
+    slug = request.user.slug
+    return redirect('accounts:user_profile', slug=slug)
