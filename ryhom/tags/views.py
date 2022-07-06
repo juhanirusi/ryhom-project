2e093b557a98374aee0cf9df3d812e6ad793fabe
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-07 00:39:34 +0300

    Added a view for tag detail.

--- ryhom/tags/views.py
+++ ryhom/tags/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from ryhom.articles.models import Article
 
-# Create your views here.
+from .models import Tag
+
+
+class TagDetailView(ListView):
+    model = Tag
+    context_object_name = 'tag'
+    template_name = 'tags/tag-page.html'
+
+    def get_queryset(self):
+        tag = Tag.objects.get(slug=self.kwargs['tag_slug'])
+        self.queryset = Article.objects.filter(tags=tag)
+        return tag
+
+    def get_context_data(self, **kwargs):
+        context = super(TagDetailView, self).get_context_data(**kwargs)
+        context['posts_in_tag'] = self.queryset
+        return context
