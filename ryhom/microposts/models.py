e1fee64df0d8d1ea99433e72827e3b04b1b1e8cd
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-08 00:27:46 +0300

    Added slug making on save & added the comment model to micropost app

--- ryhom/microposts/models.py
+++ ryhom/microposts/models.py
@@ -1,28 +1,47 @@
 from django.conf import settings
 from django.db import models
-from django.urls import reverse
+#from django.urls import reverse
 from ryhom.categories.models import Category
-from ryhom.core.models import BaseAbstractModel
+from ryhom.core.models import BaseAbstractModel, BaseCommentModel
+from ryhom.core.utils import random_string_generator
 from ryhom.tags.models import Tag
 
 
 class Micropost(BaseAbstractModel):
-
-    class Type(models.TextChoices):
-        TEXT = 'Text', 'Text'
-        IMAGE = 'Image', 'Image'
-
-    title = models.CharField(max_length=255, null=True)
+    title = models.CharField(max_length=255)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(blank=True, upload_to='micropost-images/')
+    image = models.ImageField(upload_to='micropost-images/')
     image_credit = models.CharField(blank=True, max_length=50)
-    content = models.TextField(blank=True)
+    content = models.TextField(max_length=1700)
     categories = models.ManyToManyField(Category, blank=True)
     tags = models.ManyToManyField(Tag, blank=True)
-    type = models.CharField(max_length=25, choices=Type.choices, default=Type.TEXT)
     likes = models.PositiveIntegerField(default=0)
     published = models.BooleanField(default=False)
     slug = models.SlugField(default='', blank=True, null=False, unique=True)
+
+    class Meta:
+        verbose_name = 'Micropost'
+        verbose_name_plural = 'Microposts'
+        ordering = ['-created', '-modified']
+
+
+    def _create_slug(self):
+        unique_slug = random_string_generator()
+        while Micropost.objects.filter(slug=unique_slug).exists():
+            unique_slug = random_string_generator()
+            unique_slug = '{}'.format(unique_slug)
+
+        self.slug = unique_slug
+
+
+    def save(self, *args, **kwargs):
+        """
+        Override the save method to generate a URL slug for
+        the article.
+        """
+        if self._state.adding is True:
+            self._create_slug()
+        super(Micropost, self).save(*args, **kwargs)
 
 
     # def get_absolute_url(self):
@@ -31,3 +50,31 @@ class Micropost(BaseAbstractModel):
 
     def __str__(self):
         return self.title
+
+
+class MicropostComment(BaseCommentModel):
+    post = models.ForeignKey(Micropost, on_delete=models.CASCADE)
+
+    objects = models.Manager()
+    #user_comments = UserCommentsManager()
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+        ordering=['-created']
+
+
+    @property
+    def child_comments(self):
+        return MicropostComment.objects.filter(parent=self).all()
+
+
+    @property
+    def is_parent(self):
+        if self.parent is None:
+            return True
+        return False
+
+
+    def __str__(self):
+        return f'Comment by {self.author}'
