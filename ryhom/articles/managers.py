d6077666a92d6fd825f771115ec46f48b5948674
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-09 00:11:33 +0300

    Recoded the article app's model managers / qyerysets.

--- ryhom/articles/managers.py
+++ ryhom/articles/managers.py
@@ -1,29 +1,46 @@
 from django.db import models
-from django.db.models import Q
 
 
-class UserPostsManager(models.Manager):
-    def published_articles(self, user):
-        return self.get_queryset().filter(
-            Q(status='Published') &
-            Q(author=user)).order_by('-modified')
+class ArticleQuerySet(models.QuerySet):
+    def by_author(self, author):
+        return self.filter(author=author)
 
-    def draft_articles(self, user):
-        return self.get_queryset().filter(
-            Q(status='Saved For Later') &
-            Q(author=user)).order_by('-modified')
+    def published_articles(self):
+        return self.filter(status='Published').order_by('-modified')
 
-    def waiting_review_articles(self, user):
-        return self.get_queryset().filter(
-            Q(status='Wants To Publish') &
-            Q(author=user)).order_by('-modified')
+    def draft_articles(self):
+        return self.filter(status='Saved For Later').order_by('-modified')
 
-    def user_profile_published(self, user):
-        return self.get_queryset().filter(
-            Q(status='Published') &
-            Q(author=user)).order_by('-modified')
+    def waiting_review(self):
+        return self.filter(status='Wants To Publish').order_by('-modified')
 
 
-class UserCommentsManager(models.Manager):
-    def user_profile_comments(self, user):
-        return self.get_queryset().filter(Q(author=user))
+class ArticleManager(models.Manager):
+    def get_queryset(self):
+        return ArticleQuerySet(self.model, using=self._db)
+
+    def by_author(self, author):
+        return self.get_queryset().by_author(author)
+
+    def published_articles(self):
+        return self.get_queryset().published_articles()
+
+    def draft_articles(self):
+        return self.get_queryset().draft_articles()
+
+    def waiting_review(self):
+        return self.get_queryset().waiting_review()
+
+# -------------------------------------------------------------------
+
+class ArticleCommentsQuerySet(models.QuerySet):
+    def user_comments(self, author):
+        return self.filter(author=author)
+
+
+class ArticleCommentsManager(models.Manager):
+    def get_queryset(self):
+        return ArticleCommentsQuerySet(self.model, using=self._db)
+
+    def user_comments(self, author):
+        return self.get_queryset().user_comments(author)
