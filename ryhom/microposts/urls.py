ad0ce9d9796a5b4c5ab422d47de7d35a9d622661
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-09 00:20:25 +0300

    URLs for adding microposts +and URL for micropost detail view.

--- ryhom/microposts/urls.py
+++ ryhom/microposts/urls.py
@@ -1,7 +1,10 @@
-#from django.urls import path
+from django.urls import path
 
-#from .views import
+from .views import AddMicropostView, MicropostDetailView
 
 app_name = 'microposts'
 
-urlpatterns = []
+urlpatterns = [
+    path('add-post', AddMicropostView.as_view(), name='add_micropost'),
+    path('<slug:micropost_slug>', MicropostDetailView.as_view(), name='micropost_detail'),
+]
