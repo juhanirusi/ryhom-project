91e5f753f2f81045a9fb16e30f5ea8a064692f8d
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-09 00:12:49 +0300

    Moved  my-posts URL to accounts app.

--- ryhom/articles/urls.py
+++ ryhom/articles/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import AddArticleView, ArticleDetailView, UserPostsView
+from .views import AddArticleView, ArticleDetailView
 
 app_name = 'articles'
 
 urlpatterns = [
     path('add-article', AddArticleView.as_view(), name='add_article'),
-    path('my-posts', UserPostsView.as_view(), name='my_posts'),
     path('<slug:article_slug>', ArticleDetailView.as_view(), name='article_detail'),
 
     # path('post/<int:post_pk>/comment/<int:pk>/like', AddCommentLike.as_view(), name='comment-like'),
