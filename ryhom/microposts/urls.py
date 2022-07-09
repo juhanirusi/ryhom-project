9311a6e5c444719d4528cf4ee68eff8a64707d48
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-10 00:43:05 +0300

    Added URL for listing all microposts.

--- ryhom/microposts/urls.py
+++ ryhom/microposts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import AddMicropostView, MicropostDetailView
+from .views import AddMicropostView, AllMicropostsListView, MicropostDetailView
 
 app_name = 'microposts'
 
 urlpatterns = [
+    path('all', AllMicropostsListView.as_view(), name='all_microposts'),
     path('add-post', AddMicropostView.as_view(), name='add_micropost'),
     path('<slug:micropost_slug>', MicropostDetailView.as_view(), name='micropost_detail'),
 ]
