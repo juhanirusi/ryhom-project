5cb8c9e83304da35595c6f70b7073de9dac5c7ca
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-11 00:13:51 +0300

    Added URLs for specific view for articles in tags and microposts in tags.

--- ryhom/tags/urls.py
+++ ryhom/tags/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TagDetailView
+from .views import ArticlesByTagListView, MicropostsByTagListView
 
 app_name = 'tags'
 
 urlpatterns = [
-    path('<slug:tag_slug>', TagDetailView.as_view(), name='tag_detail'),
+    path('articles/<slug:tag_slug>', ArticlesByTagListView.as_view(), name='articles_by_tag'),
+    path('microposts/<slug:tag_slug>', MicropostsByTagListView.as_view(), name='microposts_by_tag'),
 ]
