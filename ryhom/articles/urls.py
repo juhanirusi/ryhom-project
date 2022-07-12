f2138a88068237c49df5773056afe69eac6abc5a
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-12 10:40:02 +0300

    Added URLs for editing & deleting articles.

--- ryhom/articles/urls.py
+++ ryhom/articles/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from .views import AddArticleView, ArticleDetailView
+from .views import (AddArticleView, ArticleDetailView, DeleteArticleView,
+                    EditArticleView)
 
 app_name = 'articles'
 
 urlpatterns = [
     path('add-article', AddArticleView.as_view(), name='add_article'),
+    path('edit-article/<uuid:article_uuid>', EditArticleView.as_view(), name='edit_article'),
+    path('delete-article/<uuid:article_uuid>', DeleteArticleView.as_view(), name='delete_article'),
     path('<slug:article_slug>', ArticleDetailView.as_view(), name='article_detail'),
 
     # path('post/<int:post_pk>/comment/<int:pk>/like', AddCommentLike.as_view(), name='comment-like'),
