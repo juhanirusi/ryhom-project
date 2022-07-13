739b2a7c1d86caded2318bfc90790661cd3261ad
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-13 10:24:41 +0300

    URL for deleting microposts.

--- ryhom/microposts/urls.py
+++ ryhom/microposts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import AddMicropostView, AllMicropostsListView, MicropostDetailView
+from .views import (AddMicropostView, AllMicropostsListView,
+                    DeleteMicropostView, MicropostDetailView)
 
 app_name = 'microposts'
 
 urlpatterns = [
     path('all', AllMicropostsListView.as_view(), name='all_microposts'),
     path('add-post', AddMicropostView.as_view(), name='add_micropost'),
+    path('delete-post/<uuid:micropost_uuid>', DeleteMicropostView.as_view(), name='delete_post'),
     path('<slug:micropost_slug>', MicropostDetailView.as_view(), name='micropost_detail'),
 ]
