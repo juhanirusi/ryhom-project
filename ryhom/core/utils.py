b9bc3aba768c7f17a61b677798a9abe2fe11b81f
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-16 00:32:56 +0300

    Added the resize_optimize_image method to the core app's utils.py

--- ryhom/core/utils.py
+++ ryhom/core/utils.py
@@ -1,5 +1,10 @@
 import random
 import string
+import uuid
+from io import BytesIO
+
+from django.core.files import File
+from PIL import Image
 
 
 def random_string_generator(size=10, chars=True, ints=True):
@@ -25,3 +30,24 @@ def random_string_generator(size=10, chars=True, ints=True):
         random_includes = integers
 
     return ''.join(random.choice(random_includes) for _ in range(size))
+
+
+def resize_optimize_image(uploaded_image, desired_width, desired_height):
+    img = Image.open(uploaded_image)
+    (current_w, current_h) = img.size # current size (width, height)
+
+    if not img.mode == 'RGB':
+        img = img.convert('RGB')
+
+    if current_w or current_h > desired_width or desired_height:
+        new_size = (desired_width, desired_height) # new size
+        img.thumbnail(new_size, Image.ANTIALIAS)
+
+    random_name = f'{uuid.uuid4()}.jpeg'
+
+    img_io = BytesIO()
+    img.save(img_io, 'JPEG', quality=98, optimize=True)
+
+    new_image = File(img_io, name=random_name)
+
+    return new_image
