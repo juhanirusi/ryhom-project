c516547ac5f7220f9aedf2bb7a921caf51fbfb5d
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-20 00:06:20 +0300

    URLs for liking microposts and micropost comments.

--- ryhom/microposts/urls.py
+++ ryhom/microposts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import (AddMicropostView, AllMicropostsListView,
+from .views import (AddMicropostView, AddRemoveMicropostCommentLike,
+                    AddRemoveMicropostLike, AllMicropostsListView,
                     DeleteMicropostView, MicropostDetailView)
 
 app_name = 'microposts'
@@ -9,5 +10,8 @@ urlpatterns = [
     path('all', AllMicropostsListView.as_view(), name='all_microposts'),
     path('add-post', AddMicropostView.as_view(), name='add_micropost'),
     path('delete-post/<uuid:micropost_uuid>', DeleteMicropostView.as_view(), name='delete_post'),
+
     path('<slug:micropost_slug>', MicropostDetailView.as_view(), name='micropost_detail'),
+    path('<int:micropost_pk>/like', AddRemoveMicropostLike.as_view(), name='micropost_like_unlike'),
+    path('<int:micropost_pk>/comment/<int:pk>/like', AddRemoveMicropostCommentLike.as_view(), name='micropost_comment_like_unlike'),
 ]
