b0ae4412c31bae142780980c912507db230eaaa9
JuhaniRusi <juhanirusi@yahoo.com>
2022-07-20 00:04:40 +0300

    A little edit to the URLs.

--- ryhom/articles/urls.py
+++ ryhom/articles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import (AddArticleView, AddRemoveCommentLike, ArticleDetailView,
-                    DeleteArticleView, EditArticleView)
+from .views import (AddArticleView, AddRemoveArticleCommentLike,
+                    ArticleDetailView, DeleteArticleView, EditArticleView)
 
 app_name = 'articles'
 
@@ -11,5 +11,5 @@ urlpatterns = [
     path('delete-article/<uuid:article_uuid>', DeleteArticleView.as_view(), name='delete_article'),
 
     path('<slug:article_slug>', ArticleDetailView.as_view(), name='article_detail'),
-    path('<int:article_pk>/comment/<int:pk>/like', AddRemoveCommentLike.as_view(), name='comment_like_unlike'),
+    path('<int:article_pk>/comment/<int:pk>/like', AddRemoveArticleCommentLike.as_view(), name='article_comment_like_unlike'),
 ]
